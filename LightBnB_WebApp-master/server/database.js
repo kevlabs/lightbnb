96d1c51858c2dbca6f0caaf9db4625daa92987a8
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-11-14 17:24:22 -0500

    fix add resa

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,8 +78,8 @@ const getAllReservations = function(guestId, limit = 10) {
     FROM users u
     JOIN reservations r ON r.guest_id = u.id
     JOIN properties p ON r.property_id = p.id
-    JOIN property_reviews pr ON p.id = pr.property_id
-    WHERE u.id = $1 AND r.end_date < now()::date
+    LEFT JOIN property_reviews pr ON p.id = pr.property_id
+    WHERE u.id = $1
     GROUP BY p.id, r.id
     ORDER BY r.start_date DESC LIMIT $2;
   `, [guestId, limit])
